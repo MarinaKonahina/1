c0dd6c8707acf1317efba7d986a1c9446f552f68
MarinaKonahina <106992216+MarinaKonahina@users.noreply.github.com>
2022-07-09 17:08:03 +0300

    Update HW-12.7.3.py

--- HW-12.7.3.py
+++ HW-12.7.3.py
@@ -2,10 +2,10 @@ print("")
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = 100000
 summ_money = {}
-summ_money['ТКБ'] = (5.6*money)/100
-summ_money['СКБ'] = (5.9*money)/100
-summ_money['ВТБ'] = (4.28*money)/100
-summ_money['СБЕР'] = (4.0*money)/100
+summ_money['ТКБ'] = (per_cent['ТКБ']*money)/100
+summ_money['СКБ'] = (per_cent['СКБ']*money)/100
+summ_money['ВТБ'] = (per_cent['ВТБ']*money)/100
+summ_money['СБЕР'] = (per_cent['СБЕР']*money)/100
 print(summ_money)
 print(list(summ_money.values()))
 print(max(list(summ_money.values())))
